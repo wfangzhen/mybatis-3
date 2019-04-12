ab6854055830b4142779aa2881e23d3549251ed4
wangfz <1052893544@qq.com>
2019-04-12 11:47:11 +0800

    第 dd 次修改  commit

--- src/main/java/org/apache/ibatis/mapping/SqlSource.java
+++ src/main/java/org/apache/ibatis/mapping/SqlSource.java
@@ -19,6 +19,7 @@ package org.apache.ibatis.mapping;
  * Represents the content of a mapped statement read from an XML file or an annotation.
  * It creates the SQL that will be passed to the database out of the input parameter received from the user.
  * @author Clinton Begin
+ * dd dd
  */
 public interface SqlSource {
 
