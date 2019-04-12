c8c6cc670a6fee85b57ab4f7c3943128a66b6c55
wangfz <1052893544@qq.com>
2019-04-12 11:40:45 +0800

    Merge remote-tracking branch 'remotes/origin/dd'
    # Conflicts:
    #	src/main/java/org/apache/ibatis/mapping/SqlSource.java

--- src/main/java/org/apache/ibatis/mapping/SqlSource.java
+++ src/main/java/org/apache/ibatis/mapping/SqlSource.java
@@ -18,7 +18,6 @@ package org.apache.ibatis.mapping;
 /**
  * Represents the content of a mapped statement read from an XML file or an annotation.
  * It creates the SQL that will be passed to the database out of the input parameter received from the user.
- *   的的的鹅鹅鹅饿鹅鹅鹅饿
  * @author Clinton Begin
  */
 public interface SqlSource {
